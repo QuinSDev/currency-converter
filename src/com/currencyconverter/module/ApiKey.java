ab9f4f6ca275141c4e8f16cdb7e68db4b170b0cd
QuinSDev <quindev97@gmail.com>
2024-04-26 13:17:57 -0500

    Se crea el método que lee el archivo que contiene la api key y la extrae para poder usarla.

--- src/com/currencyconverter/module/ApiKey.java
+++ src/com/currencyconverter/module/ApiKey.java
@@ -1,2 +1,26 @@
-package com.currencyconverter.module;public class ApiKey {
+package com.currencyconverter.module;
+
+import java.io.*;
+
+public class ApiKey {
+
+    private String apiKey;
+
+    public ApiKey() {
+        loadApiKey();
+    }
+
+    private void loadApiKey() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("api_key.txt"))) {
+            String apiKey = reader.readLine();
+            this.apiKey = apiKey.trim();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
 }
